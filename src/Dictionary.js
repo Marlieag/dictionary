88b660d277f1255752425acf992505085a9e22c5
Marlieag <Marlieag@users.noreply.github.com>
2021-06-04 19:21:23 -0600

    rendered the word definition

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary (){
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
  
 function handleResponse(response){
-    console.log(response);
+    console.log(response.data[0]);
+    setResults(response.data[0]);
 }
 function search (event){
     event.preventDefault();
-    alert (`Searching for ${keyword}`);
-
-let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_us/${keyword}`;
+let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
 axios.get(apiUrl).then(handleResponse);
 }
 function handleKeywordChange (event){
@@ -39,9 +40,10 @@ function handleKeywordChange (event){
             />
         </div>
             </div>
+        <Results results= {results}/>
         </form>
         </div>
         </div>
-    </div>
+    </div> 
     )
 }
