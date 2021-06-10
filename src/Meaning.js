2f9537b47076c84872a9c23f8ec3f9750a0d4cb9
Marlieag <Marlieag@users.noreply.github.com>
2021-06-09 18:48:53 -0600

    Synonyms added

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning (props){
     console.log(props.meaning);
@@ -11,11 +12,18 @@ export default function Meaning (props){
                 return(
                     <div key={index}>
                     <p>
+                        <strong>
+                            Definition:{""}
+                        </strong>
                         {definition.definition}
                     </p>
                     <p>
+                        <strong>
+                            Example: {""}
+                        </strong>
                     <em> {definition.example}</em> 
                     </p>
+                    <Synonyms synonyms={definition.synonyms}/>
                     </div>
                 );
             })}
---
+++ src/Synonyms.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Synonyms(props){
+    if (props.synonyms){
+    return (
+        <div className ="synonyms">
+            <ul>
+        {props.synonyms.map(function(synonym, index) {
+            return (
+                <span key={index}>
+                <li>
+                {synonym}
+                </li>
+                </span>
+            );
+            
+        })}  
+            </ul> 
+        </div>
+    );
+    } else {
+        return null;
+    }
+}
